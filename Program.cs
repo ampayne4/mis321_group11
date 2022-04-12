570edcfff42854dd0256de5118305ceae0ae9732
Betsy Bryant <betsybryant@Betsys-MacBook-Air.local>
2022-04-11 20:05:35 -0500

    Betsyyy

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace mis321_group11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi World!");
         }
     }
 }
