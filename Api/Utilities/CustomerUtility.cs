7f56217e73cca683db68acabc2691c1db13742e6
MonteriaWilson <78232403+MonteriaWilson@users.noreply.github.com>
2022-04-18 20:52:58 -0500

    idk

---
+++ Api/Utilities/CustomerUtility.cs
@@ -0,0 +1,31 @@
+namespace Api.Properties
+{
+    public class DatabaseUtility
+    {
+        public void CreateUser()
+        {
+
+        }
+
+        public void UserLogin()
+        {
+
+        }
+        public void Payment()
+        {
+
+        }
+        public void DeleteProduct()
+        {
+
+        }
+        public void AddProduct()
+        {
+
+        }
+        public void DisplayPastOrders()
+        {
+
+        }
+    }
+}
