d8af471c11795c49914961f1502f8bb2bcea096d
MonteriaWilson <78232403+MonteriaWilson@users.noreply.github.com>
2022-04-18 20:16:12 -0500

    connection string

---
+++ Api/connectionstring.cs
@@ -0,0 +1,18 @@
+namespace Api
+{
+    public class connectionstring
+    {
+        public string cs {get;set;}
+
+        public connectionstring()
+        {
+            string server = "w3epjhex7h2ccjxx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+            string database = "ggufxyz547q4nwo0";
+            string port = "3306";
+            string username = "vut7hg87owhj0qg9";
+            string password = "pezr9x42bzm4ra5p";
+
+            cs = $@"server = {server}; user = {username}; database = {database}; port = {port}; password={password}; convert zero datetime =True";
+        }
+    }
+}
