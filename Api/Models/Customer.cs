5b701b0b08c916be9eb50aeeb07a64a37ebd824f
Betsy Bryant <betsybryant@Betsys-MacBook-Air.local>
2022-04-25 15:05:46 -0500

    update

--- Api/Models/Customer.cs
+++ Api/Models/Customer.cs
@@ -2,14 +2,14 @@ namespace Api.Models
 {
     public class Customer
     {
-        public int Id {get; set;}
-        public string FirstName{get;set;}
-        public string LastName{get;set;}
-        public string Email{get;set;}
-        public string Phone{get;set;}
-        public string Password{get;set;}
-         public int CardNo {get;set;}
+        public int CustId {get; set;}
+        public string CustFName{get;set;}
+        public string CustLName{get;set;}
+        public string CustEmail{get;set;}
+        public string CustPassword{get;set;}
+        public string CardName {get; set;}
+        public int CardNo {get;set;}
         public int CvvNo {get;set;}
-        public string Date{get;set;}
+        public string CdDate{get;set;}
     }
 }
